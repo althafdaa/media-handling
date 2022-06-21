49a59db670759279c9a4909a9a504c7610f9bc8a
althafdemiandra <100656575+althafdemiandra@users.noreply.github.com>
2022-06-21 21:44:40 +0700

    fixbug

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,10 @@
 import Head from 'next/head';
 import styles from '../../styles/Home.module.css';
-import { BigPlayButton, Player, VolumeMenuButton } from 'video-react';
+import { BigPlayButton, Player } from 'video-react';
 import { useRef } from 'react';
 
 export default function Home() {
   const videoRef = useRef(null);
-
-  const { video } = videoRef.current;
 
   return (
     <div className={styles.container}>
@@ -20,13 +18,20 @@ export default function Home() {
           ref={videoRef}
           src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
           poster="https://images.unsplash.com/photo-1431440869543-efaf3388c585?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+          autoPlay
         >
           <BigPlayButton position="center" />
         </Player>
 
-        <button onClick={() => video.play()}>custom Play</button>
-        <button onClick={() => video.pause()}>custom pause</button>
-        <button onClick={() => video.toggleFullscreen()}>full screen</button>
+        <button onClick={() => videoRef?.current?.video?.play()}>
+          custom Play
+        </button>
+        <button onClick={() => videoRef?.current?.video?.pause()}>
+          custom pause
+        </button>
+        <button onClick={() => videoRef?.current?.video?.toggleFullscreen()}>
+          full screen
+        </button>
       </div>
     </div>
   );
